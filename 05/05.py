93878f7b14928a61f39922071a73701c19e0a8d0
Job Ammerlaan <job.ammerlaan@randstad.com>
2022-12-05 17:55:36 +0100

    Day 5 is also very ugly, but it works. I'll rework all this tomorrow :)

--- 05/05.py
+++ 05/05.py
@@ -4,29 +4,28 @@ lines = read_input_file(day='05', output_type='string')
 
 
 def read_input(lines):
-    drawing, instructions = lines.split('\n\n')
-    instructions = instructions.split('\n')
-    drawing_lines = drawing.split('\n')
+    drawing, instruction_lines = lines.split('\n\n')
 
+    drawing_lines = drawing.split('\n')
     stacks = [list() for _ in drawing_lines.pop().split()]
     for line in reversed(drawing_lines):
         for i in range(len(stacks)):
             if 4 * i + 1 < len(line) and line[4 * i + 1] != ' ':
                 stacks[i].append(line[4 * i + 1])
+
+    instructions = [[int(i) for i in line.split(' ')[1::2]] for line in instruction_lines.split('\n')]
     return stacks, instructions
 
 
 def part_one(stacks, instructions):
-    for instruction in instructions:
-        qty, src, target = [int(i) for i in instruction.split(' ')[1::2]]
+    for qty, src, target in instructions:
         for _ in range(qty):
             stacks[target - 1].append(stacks[src - 1].pop())
     print(''.join(stack[len(stack) - 1] for stack in stacks))
 
 
 def part_two(stacks, instructions):
-    for instruction in instructions:
-        qty, src, target = [int(i) for i in instruction.split(' ')[1::2]]
+    for qty, src, target in instructions:
         temp_stack = []
         for _ in range(qty):
             temp_stack.append(stacks[src - 1].pop())
