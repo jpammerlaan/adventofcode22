a9a8b7d98e5e18f34efd57e709414cf1362530e9
Job Ammerlaan <job.ammerlaan@randstad.com>
2022-12-07 09:55:48 +0100

    You can just slice forward in day 6 of course...

--- 06/06.py
+++ 06/06.py
@@ -4,9 +4,9 @@ input_buffer = read_input_file(day='06', output_type='string')
 
 
 def find_marker(buffer, marker_length):
-    for i, c in enumerate(buffer):
-        if i > marker_length - 1 and len(set(buffer[i-marker_length:i])) == marker_length:
-            return i
+    for i in range(len(buffer)):
+        if len(set(buffer[i:i + marker_length])) == marker_length:
+            return i + marker_length
 
 
 def part_one(buffer):
