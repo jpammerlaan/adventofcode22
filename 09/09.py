751a64514a40762326a794134c1de1e8640a9726
Job Ammerlaan <job.ammerlaan@randstad.com>
2022-12-09 12:20:35 +0100

    Remove obsolete print functions, combine parts one and two

--- 09/09.py
+++ 09/09.py
@@ -12,46 +12,6 @@ DIRECTION_MAP = {
 }
 
 
-def print_visited(visited, minx=0, miny=0, maxx=6, maxy=5):
-    for y in reversed(range(miny, maxy)):
-        for x in range(minx, maxx):
-            print(' ', end='')
-            if (x, y) in visited:
-                print('#', end='')
-            else:
-                print('.', end='')
-        print('')
-
-
-def print_knots(knots, minx=0, miny=0, maxx=6, maxy=5):
-    for y in reversed(range(miny, maxy)):
-        for x in range(minx, maxx):
-            print(' ', end='')
-            printed = False
-            for i, knot in enumerate(knots):
-                if (x, y) == tuple(knot):
-                    print(i, end='')
-                    printed = True
-                    break
-            if not printed:
-                print('.', end='')
-        print('')
-    print('\n')
-
-
-def part_one(instructions):
-    head = np.array((0., 0.))
-    tail = np.array((0., 0.))
-    visited = set()
-    for direction, n_steps in instructions:
-        step = DIRECTION_MAP[direction]
-        for i in range(n_steps):
-            head += step
-            tail = get_tail(head, tail)
-            visited.add(tuple(tail))
-    print(len(visited))
-
-
 def get_tail(head, tail):
     if np.linalg.norm(head - tail) > np.sqrt(2):
         dx, dy = head - tail
@@ -62,8 +22,8 @@ def get_tail(head, tail):
     return tail
 
 
-def part_two(instructions):
-    knots = [np.array((0., 0.)) for _ in range(10)]  # init 9 knots on 0,0
+def part_one(instructions, n_knots):
+    knots = [np.array((0., 0.)) for _ in range(n_knots + 1)]  # init 9 knots on 0,0
     visited = set()
     for direction, n_steps in instructions:
         step = DIRECTION_MAP[direction]
@@ -71,9 +31,9 @@ def part_two(instructions):
             knots[0] += step
             for k, tail in enumerate(knots[1:]):
                 knots[k + 1] = get_tail(knots[k], knots[k + 1])
-            visited.add(tuple(knots[9]))
+            visited.add(tuple(knots[n_knots]))
     print(len(visited))
 
 
-part_one(instructions=instruction_list)
-part_two(instructions=instruction_list)
+part_one(instructions=instruction_list, n_knots=1)
+part_one(instructions=instruction_list, n_knots=9)
